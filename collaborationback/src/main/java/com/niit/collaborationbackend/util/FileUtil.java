4d30ac1df2cb7a278046c6b4ab7963cbd552199c
Rutuja <rutu956177@gmail.com>
2017-02-03 00:57:01 +0530

    friend module completed

--- collaborationback/src/main/java/com/niit/collaborationbackend/util/FileUtil.java
+++ collaborationback/src/main/java/com/niit/collaborationbackend/util/FileUtil.java
@@ -1,6 +1,7 @@
 package com.niit.collaborationbackend.util;
 
 import java.io.BufferedOutputStream;
+
 import java.io.File;
 import java.io.FileOutputStream;
 
@@ -10,49 +11,42 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
-
-
 public class FileUtil {
 	public static Logger log = LoggerFactory.getLogger(FileUtil.class.getName());
 
-/*	private static String path = "D:/Collaboration/collaborationback/src/main/webapp/Images/";
-*/	
-	private static String path ="E:/2694/CollaborationFront/WebContent/Images/";
 	
-	public static void Upload(MultipartFile file,String filename)
-	{
-		log.debug("FileUtil ---> Starting of the Method Upload()");
-		
+	private static String path = "E:/2694/CollaborationFront/WebContent/images/";
 
-		//HttpServletRequest request;
-		
-/*		Sting upload = request.get
-*/		if(!file.isEmpty()){
-			
-			try{
+	public static void Upload(MultipartFile file, String filename) {
+		log.debug("FileUtil ---> Starting of the Method Upload()");
+
+		// HttpServletRequest request;
+
+		/*
+		 * Sting upload = request.get
+		 */ if (!file.isEmpty()) {
+
+			try {
 				byte[] bytes = file.getBytes();
 				File dir = new File(path);
-				if(!dir.exists())
-				{
+				if (!dir.exists()) {
 					dir.mkdirs();
 				}
-				File ServerFile = new File(dir.getAbsolutePath()+File.separator +filename);
+				File ServerFile = new File(dir.getAbsolutePath() + File.separator + filename);
 
 				BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(ServerFile));
-			stream.write(bytes);
-			stream.close();
-			log.debug("FileUtil ---> Ending of the Method Upload()");
+				stream.write(bytes);
+				stream.close();
+				log.debug("FileUtil ---> Ending of the Method Upload()");
 
-			}
-catch(Exception e)
-			{
-	e.printStackTrace();
-	System.out.println(e);
-	log.debug("FileUtil ---> Ending of the Method DisplayAllCart()");
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println(e);
+				log.debug("FileUtil ---> Ending of the Method Upload()");
 
 			}
 		}
-		
+
 	}
-	
-	}
+
+}
